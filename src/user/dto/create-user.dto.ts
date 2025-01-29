f023f03bd41813bc60234c87b3bea1d39a4ba6e1
dhouhahmd <dhouhahamoudi@gmail.com>
2025-01-29 19:46:31 +0100

    Ajout des nouvelles modifications

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,11 @@
+export class CreateUserDto {
+    id: number;
+    firstName: string;
+    lastName: string;
+    password: string;
+    email: string;
+    role: string;
+    telephone:number;
+    
+  
+}
